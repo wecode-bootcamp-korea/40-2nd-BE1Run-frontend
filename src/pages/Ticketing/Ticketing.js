e8b3837bb14ae184bcc57e2c0dc5cef8dbc61121
minkyung_kwak <ellen10min@gmail.com>
2022-12-28 21:45:52 +0900

    ADD: 예매 페이지 레이아웃 완료

--- src/pages/Ticketing/Ticketing.js
+++ src/pages/Ticketing/Ticketing.js
@@ -1,7 +1,134 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Movie from './section/Movie/Movie';
+import Theater from './section/Theater/Theater';
+import Date from './section/Date/Date';
+import Time from './section/Time/Time';
+import Ticket from './section/Ticket/Ticket';
+import styled from 'styled-components';
 
 const Ticketing = () => {
-  return <div>티케팅페이지입니다</div>;
+  const [list, setList] = useState([]);
+  const [movieId, setMovieId] = useState(0);
+  const [activeLocation, setActiveLocation] = useState('');
+  const [activeTime, setActiveTime] = useState('');
+
+  return (
+    <div>
+      <Container>
+        <Title>예매</Title>
+        <ContainBox>
+          <ResetBtn onClick={() => window.location.replace('/ticketing')}>
+            <Icon className="fa-solid fa-arrow-rotate-left" />
+            &nbsp;예매 다시하기
+          </ResetBtn>
+          <Wrapper>
+            <Section>
+              <MovieClass>
+                <Movie />
+              </MovieClass>
+            </Section>
+            <Section>
+              <TheaterClass>
+                <Theater />
+              </TheaterClass>
+              <DateClass>
+                <Date
+                  Ticketing={Ticketing}
+                  movieId={movieId}
+                  activeLocation={activeLocation}
+                />
+              </DateClass>
+            </Section>
+            <Section>
+              <TimeContainer>
+                <TimeClass>
+                  <Time
+                    activeTime={activeTime}
+                    setActiveTime={setActiveTime}
+                    movieId={movieId}
+                  />
+                </TimeClass>
+              </TimeContainer>
+            </Section>
+          </Wrapper>
+        </ContainBox>
+        <Ticket />
+      </Container>
+    </div>
+  );
 };
 
 export default Ticketing;
+
+const Container = styled.h1`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ContainBox = styled.div`
+  width: 1500px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: space-around;
+  margin: 100px 0 30px 250px;
+  font-size: 30px;
+`;
+
+const ResetBtn = styled.button`
+  width: 180px;
+  height: 50px;
+  margin: 0 120px 20px 1130px;
+  border: 1px solid black;
+  border-radius: 6px;
+  background-color: #333333;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const Icon = styled.i`
+  font-size: 1rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 1205px;
+  border: 2px solid black;
+  margin: 0 auto 30px auto;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MovieClass = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const TheaterClass = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TimeContainer = styled.div`
+  display: flex;
+`;
+
+const DateClass = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TimeClass = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
---
+++ src/pages/Ticketing/section/Date/Date.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+const Movie = ({ Ticketing }) => {
+  const [selectDay, setSelectDay] = useState(false);
+  const handleDayClick = (selectDay, { selected }) => {
+    if (selected) {
+      setSelectDay(undefined);
+    } else {
+      setSelectDay(selectDay);
+    }
+  };
+  console.log(selectDay);
+
+  const today = new window.Date();
+
+  const impossibleDays = [
+    {
+      from: new window.Date(1999, 1, 10),
+      to: today.setDate(today.getDate() - 1),
+    },
+    {
+      from: today.setDate(today.getDate() + 6),
+      to: today.setDate(today.getDate() + 10000),
+    },
+  ];
+  Ticketing(selectDay);
+  return (
+    <DateContainer>
+      <Title>날짜</Title>
+      <DateBox>
+        <DayPicker
+          mode="single"
+          selected={selectDay}
+          onSelect={setSelectDay}
+          onChange={date => setSelectDay(date)}
+          onDayClick={handleDayClick}
+          disabled={impossibleDays}
+        />
+      </DateBox>
+    </DateContainer>
+  );
+};
+
+export default Movie;
+
+const DateContainer = styled.div`
+  width: 400px;
+  background-color: antiquewhite;
+`;
+
+const Title = styled.div`
+  height: 40px;
+  padding: 10px 0;
+  border: 0.5px solid gray;
+  border-top: none;
+  background-color: #333333;
+  color: white;
+  font-size: 20px;
+  text-align: center;
+`;
+
+const DateBox = styled.div`
+  height: 350px;
+  width: 400px;
+  &&& {
+    .DayPicker,
+    .Div {
+      :focus {
+        outline: none;
+      }
+    }
+    .rdp {
+      --rdp-cell-size: 45px;
+      --rdp-accent-color: black;
+      --rdp-background-color: white;
+      --rdp-outline: 0px solid gray;
+      --rdp-outline-selected: 1px solid gray;
+      margin: 5px 60px;
+    }
+    .rdp-weeknumber,
+    .rdp-day {
+      height: 30px;
+      display: flex;
+      overflow: hidden;
+      align-items: center;
+      justify-content: center;
+      box-sizing: border-box;
+      width: 30px;
+      max-width: 30px;
+      margin: 0;
+    }
+  }
+`;
