e68b61d937bf7c19c3a9e28efe213c9abb014dd1
Oh9yu <kog6148@gmail.com>
2022-12-29 21:11:08 +0900

    Add: orderPage 레이아웃 완료

---
+++ src/pages/OrderPage/ORDERDATA.js
@@ -0,0 +1,15 @@
+//TODO : 목데이터 변경
+
+const ORDER_DATA = {
+  img: 'images/android-g298777c61_1280.png',
+  headerCount: 2,
+  exp: [
+    { value: '아바타 - 물의길' },
+    { value: 'BMT 강남' },
+    { value: '2관 6층' },
+    { value: '2022년 12월 30일' },
+    { value: 'E10, E11' },
+  ],
+};
+
+export default ORDER_DATA;
---
+++ src/pages/OrderPage/OrderTitleData.js
@@ -0,0 +1,9 @@
+const ORDER_TITLE_DATA = [
+  { id: 1, value: '영화명' },
+  { id: 2, value: '극장' },
+  { id: 3, value: '상영관' },
+  { id: 4, value: '상영일' },
+  { id: 5, value: '좌석' },
+];
+
+export default ORDER_TITLE_DATA;
