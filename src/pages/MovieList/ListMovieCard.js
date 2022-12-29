344f051d4578a013f544d3cd37073657df39cac8
unknown <shinch8803@gmail.com>
2022-12-29 19:09:02 +0900

    FIX: 영화리스트 레이아웃 완성

---
+++ src/pages/MovieList/ListMovieCard.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const ListMovieCard = props => {
+  const { id, url, name, runtime } = props;
+  const navigate = useNavigate();
+
+  const onClickTiketingButton = () => {
+    navigate(`/tiketing/${id}`);
+  };
+  const onClickDetailButton = () => {
+    navigate(`/moviedetail/${id}`);
+  };
+
+  return (
+    <Container>
+      <MovieNumber>No.{id}</MovieNumber>
+      <MovieThumnail>
+        <PosterThumnail
+          src={url}
+          onClick={onClickDetailButton}
+          alt="영화썸네일"
+        />
+      </MovieThumnail>
+      <MovieName>{name}</MovieName>
+      <MovieRunTime>{runtime}</MovieRunTime>
+      <TicketingButton onClick={onClickTiketingButton}>
+        예매하기
+      </TicketingButton>
+    </Container>
+  );
+};
+
+export default ListMovieCard;
+
+const Container = styled.div`
+  width: 220px;
+  padding-bottom: 40px;
+  text-align: left;
+`;
+
+const MovieNumber = styled.div`
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 5px;
+  background-color: red;
+  border-radius: 10px;
+`;
+
+const MovieThumnail = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+`;
+
+const PosterThumnail = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const TicketingButton = styled.button`
+  width: 110px;
+  height: 30px;
+  color: white;
+  background-color: black;
+  top: 130px;
+  left: 40px;
+  border-radius: 10px;
+  cursor: pointer;
+  :hover {
+    opacity: 40%;
+  }
+`;
+
+const MovieName = styled.div`
+  padding-top: 20px;
+  font-size: 25px;
+`;
+
+const MovieRunTime = styled.div`
+  padding-top: 10px;
+  padding-bottom: 10px;
+`;
--- src/pages/MovieList/MovieList.js
+++ src/pages/MovieList/MovieList.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import ListMovieCard from './ListMovieCard';
 
 const MovieList = () => {
-  return <div>movieList입니다</div>;
+  const [movieCard, setMovieCard] = useState([]);
+  useEffect(() => {
+    fetch('/data/MovieList.json')
+      .then(result => result.json())
+      .then(data => setMovieCard(data));
+  }, []);
+
+  return (
+    <Container>
+      <Contents>
+        <MovieListTitle>
+          <h1>무비차트</h1>
+          <Link to="#">▶무비차트</Link>
+        </MovieListTitle>
+        <MovieListSort>
+          <Select>
+            <option value="highTiketing">예매율순</option>
+            <option value="highScore">평점순</option>
+            <option value="viewPeople">관람객순</option>
+          </Select>
+        </MovieListSort>
+        <MovieChart>
+          {movieCard.map(movieCards => {
+            return (
+              <ListMovieCard
+                key={movieCards.id}
+                id={movieCards.id}
+                name={movieCards.name}
+                url={movieCards.url}
+                runtime={movieCards.runtime}
+              />
+            );
+          })}
+        </MovieChart>
+      </Contents>
+    </Container>
+  );
 };
 
 export default MovieList;
