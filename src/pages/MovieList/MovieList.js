89a6f9d068f69fa5f663e55d218d9706fb038417
unknown <shinch8803@gmail.com>
2022-12-29 21:13:59 +0900

    Fix: 컴플리트해결및 리베이스

--- src/pages/MovieList/MovieList.js
+++ src/pages/MovieList/MovieList.js
@@ -44,3 +44,49 @@ const MovieList = () => {
 };
 
 export default MovieList;
+
+const Container = styled.div`
+  color: white;
+  background-color: #333;
+`;
+
+const Contents = styled.div`
+  width: 70%;
+  margin: auto;
+`;
+
+const MovieListTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 3vh;
+  padding-bottom: 3vh;
+  border-bottom: solid;
+
+  h1 {
+    font-size: 5vh;
+    font-weight: bold;
+  }
+  a {
+    text-decoration-line: none;
+    padding-top: 2vh;
+    color: white;
+  }
+`;
+
+const MovieListSort = styled.div`
+  text-align: right;
+  margin-top: 1vh;
+`;
+
+const MovieChart = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 30px;
+  padding-top: 30px;
+`;
+
+const Select = styled.select`
+  width: 100px;
+  height: 30px;
+  background: white;
+`;
